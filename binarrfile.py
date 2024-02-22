db541f752d1c15d7d5b6a4ec866973d8b4aa6c80
bijaya-dulal <dulalbijaya0@gmail.com>
2024-02-22 22:56:42 +0545

    file append perform

--- binarrfile.py
+++ binarrfile.py
@@ -35,3 +35,29 @@ print("from multiple_arrays.npyz")
 print("Loaded arr1:", loaded_arr1)
 print("Loaded arr2:", loaded_arr2)
 print("Loaded arr2:", loaded_arr3)
+
+#we can append the file in this way
+# Create some additional sample NumPy arrays
+arr4 = np.linspace(0, 1, 10)
+arr5 = np.eye(4)
+
+# Append additional arrays to the existing .npz file
+with np.load('multiple_arrays.npz') as data:
+    np.savez('multiple_arrays.npz', **data, arr4=arr4, arr5=arr5)
+
+# Load the arrays from the updated file
+data = np.load('multiple_arrays.npz')
+
+# Access the loaded arrays by name
+loaded_arr1 = data['arr1']
+loaded_arr2 = data['arr2']
+loaded_arr3 = data['arr3']
+loaded_arr4 = data['arr4']
+loaded_arr5 = data['arr5']
+print("the appende file output")
+# Print the loaded arrays
+print("Loaded arr1:", loaded_arr1)
+print("Loaded arr2:", loaded_arr2)
+print("Loaded arr3:", loaded_arr3)
+print("Loaded arr4:", loaded_arr4)
+print("Loaded arr5:", loaded_arr5)
