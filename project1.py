ee74a8ce2110f5469b794c4777f3fe08d718205d
bijaya-dulal <dulalbijaya0@gmail.com>
2024-02-24 21:36:49 +0545

    image read

---
+++ project1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+#reading the image
+image = cv2.imread('image1.jpg')
+#display image
+#convert to gray scale
+#apply filter
+#combine filter
+#display processed image
+#user interaction
