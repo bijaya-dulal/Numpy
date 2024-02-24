ad1ee72ada90b31b7f992255f1c82cfbe5829dc0
bijaya-dulal <dulalbijaya0@gmail.com>
2024-02-24 18:38:27 +0545

    random module

---
+++ randomNP.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+# Generate a random array of shape (3, 3) from a standard normal distribution
+random_array = np.random.randn(3, 3)
+print("Random Array (Normal Distribution):\n", random_array)
+
+# Generate a random array of shape (3, 3) from a uniform distribution over [0, 1)
+uniform_array = np.random.rand(3, 3)
+print("\nRandom Array (Uniform Distribution):\n", uniform_array)
+
+# Generate a random integer between 0 and 9
+random_int = np.random.randint(0, 10)
+print("\nRandom Integer:", random_int)
+
+# Shuffle a sequence
+sequence = [1, 2, 3, 4, 5]
+np.random.shuffle(sequence)
+print("\nShuffled Sequence:", sequence)
