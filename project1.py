531f0699cff9f45316f8f9999bb1647e6965da6a
bijaya-dulal <dulalbijaya0@gmail.com>
2024-02-24 21:42:25 +0545

    display image

--- project1.py
+++ project1.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 
 #reading the image
 image = cv2.imread('image1.jpg')
+
 #display image
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.show()
 #convert to gray scale
 #apply filter
 #combine filter
