30a05542926df0531ee42767406b301c0464cd34
bijaya-dulal <dulalbijaya0@gmail.com>
2024-02-24 21:59:48 +0545

    gray sacle added

--- project1.py
+++ project1.py
@@ -9,8 +9,18 @@ image = cv2.imread('image1.jpg')
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.show()
+plt.title('Original Image')
 #convert to gray scale
+def rgb_to_grayscale(rgb_img):
+    # Convert RGB image to grayscale
+    grayscale_img = np.dot(rgb_img[..., :3], [0.2989, 0.5870, 0.1140])
+    return grayscale_img
+gray_image = rgb_to_grayscale(image)
+plt.subplot(1, 2, 2)
+plt.imshow(gray_image, cmap='gray')
+plt.title('gray Image')
+plt.show()
+
 #apply filter
 #combine filter
 #display processed image
